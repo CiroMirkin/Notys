aba4adbbbdd997acb98726147bb885da4127a8eb
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-10 13:32:53 -0300

    feat: integrar Apollo Provider en layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Auth0Provider } from '@auth0/nextjs-auth0/client'
+import { ApolloProviderWrapper } from '@/components/apollo-provider'
 import Navbar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,17 +22,19 @@ export default function RootLayout({
     <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
         <Auth0Provider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navbar />
-            <main className="min-h-screen">
-              {children}
-            </main>
-          </ThemeProvider>
+          <ApolloProviderWrapper>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navbar />
+              <main className="min-h-screen">
+                {children}
+              </main>
+            </ThemeProvider>
+          </ApolloProviderWrapper>
         </Auth0Provider>
       </body>
     </html>
